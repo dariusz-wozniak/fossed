7c8f125a89387b165d7f1bcc67beb28725ff26f9
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2025-04-09 17:21:31 +0200

    feat: add favicon support and update layout metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,18 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "FOSSED: Navigating .NET library licensing changes",
   description: "Tracking the changing landscape   of .NET library licenses.",
+  icons: {
+    icon: [
+      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: { url: '/favicon/apple-touch-icon.png', sizes: '180x180' },
+    other: [
+      { rel: 'android-chrome-192x192', url: '/favicon/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      { rel: 'android-chrome-512x512', url: '/favicon/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
+      { rel: 'manifest', url: '/favicon/site.webmanifest' }
+    ],
+  },
 };
 
 export default function RootLayout({
